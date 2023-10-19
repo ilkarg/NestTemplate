28954c8665674d3e47fc14b607f75de00e875732
mrProger <ilya.kargapolov02@mail.ru>
2023-10-19 22:43:57 +0600

    add cookie and session examples

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { DatabaseModule } from './database.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { SessionModule } from './modules/session/session.module';
+import { CookieModule } from './modules/cookie/cookie.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule],
+  imports: [DatabaseModule, AuthModule, SessionModule, CookieModule],
   controllers: [AppController],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: '123zcbmnvx123',
+      resave: false,
+      saveUninitialized: false
+    })
+  );
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
